6f7412288a1d1dc00c9d2831adeb8247b3bc2bf8
tsnanh <tsnanh0812@gmail.com>
2022-12-01 20:34:16 +0700

    Advent of Code in Kotlin Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,20 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .joinToString(separator = "\n")
+            .split("\n\n")
+            .maxOfOrNull { it.split("\n").sumOf { value -> value.toInt() } }!!
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .joinToString(separator = "\n")
+            .split("\n\n")
+            .map { it.split("\n").sumOf { value -> value.toInt() } }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
